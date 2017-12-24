caff59d6233f7cace8f788b9b1c63f84adea2874
邵博超DEXION <dexter0ion@gmail.com>
2017-12-24 10:17:29 +0800

    warshall可达性判断

--- InternetGraph/GraphMatrix.h
+++ InternetGraph/GraphMatrix.h
@@ -5,6 +5,7 @@
 #include "GraphNode.h"
 #include "GraphEdge.h"
 #include "vector"
+
 using namespace std;
 
 class GraphMatrix {
@@ -24,17 +25,9 @@ class GraphMatrix {
                     M[i][j] = EmptyEdge;
                 }
 
-            //初始化矩阵
-
-            Path.resize(NodeNum);
+            WarshallM.resize(NodeNum);
             for(int i=0; i<NodeNum; i++) {
-                Path[i].resize(NodeNum);
-            }
-
-
-            Dis.resize(NodeNum);
-            for(int i=0; i<NodeNum; i++) {
-                Dis[i].resize(NodeNum);
+                WarshallM[i].resize(NodeNum);
             }
         }
 
@@ -82,9 +75,9 @@ class GraphMatrix {
         void SetConnect() {
             string node1name,node2name;
             cout<<"建立连接..."<<endl;
-            cout<<"输入节点1名称：";
+            cout<<"输入起始节点名称：";
             cin>>node1name;
-            cout<<"输入节点2名称：";
+            cout<<"输入终点节点名称：";
             cin>>node2name;
 
             GraphNode node1 = SearchNodeName(node1name);
@@ -96,7 +89,6 @@ class GraphMatrix {
 
                 //建立边
                 GraphEdge Link = GraphEdge(node1,node2,1);
-                Link.SetWeight(1);
                 //关联矩阵
                 int node1ID = GetNodeId(node1name);
                 int node2ID = GetNodeId(node2name);
@@ -104,14 +96,56 @@ class GraphMatrix {
                 if(node2ID!=-1&&node1ID!=-1) {
                     M[node1ID][node2ID] = Link;
                     M[node2ID][node1ID] = Link;
-
                 }
 
             }
 
-
         }
+        void Warshall_beta() {
+            int M0[M.size()][M.size()],M1[M.size()][M.size()];
+            for(int i=0; i<M.size(); i++) {
+                for(int j=0; j<M[i].size(); j++) {
+                    if(M[i][j].getConnect())
+                        M0[i][j]=1;
+                    else if(!M[i][j].getConnect())
+                        M0[i][j]=0;
+                }
+            }
+            for(int k=0; k<M.size(); k++) {
+                for(int i=0; i<M.size(); i++) {
+                    for(int j=0; j<M.size(); j++) {
+                        if(M0[i][k]==1&&M0[k][j]==1)
+                            M1[i][j]=1;
+                        else
+                            M1[i][j]=0;
+                        if(M1[i][j]+M0[i][j]>=1)
+                            M1[i][j]=1;
+                        M0[i][j] = M1[i][j];
+                    }
+                }
+            }
 
+            //set WarshallM
+            for(int i=0; i<M.size(); i++) {
+                for(int j=0; j<M.size(); j++) {
+                    WarshallM[i][j] = M1[i][j];
+                }
+            }
+            /*
+            cout<<setw(4)<<" ";
+            for(int k=0; k<M.size(); k++)
+                cout<<setw(4)<<"N"<<k;
+            cout<<endl;
+            for(int i=0; i<M.size(); i++) {
+                cout<<setw(4)<<"N"<<i;
+                for(int j=0; j<M[i].size(); j++) {
+                    cout<<setw(4)<<M1[i][j];
+                    cout<<" ";
+                }
+                cout<<endl;
+            }
+            */
+        }
         void DisplayMatrix() {
             cout<<setw(4)<<" ";
             for(int k=0; k<M.size(); k++)
@@ -132,89 +166,29 @@ class GraphMatrix {
             }
         }
 
-
-        //最短路
-        //floyd
-        void ShortestPath_Floyd() {
-            //DistancMatrix路径长度
-            int INF = 32767;
-
-            //初始化
-            for(int i=0; i<NodeNum; i++) {
-                for(int j=0; j<NodeNum; j++) {
-                    if(i==j)
-                        Dis[i][j]=0;
-                    else
-                        Dis[i][j] = INF;
-                    Path[i][j] = -1;
+        void DisplayWarshallMatrix() {
+            cout<<setw(4)<<" ";
+            for(int k=0; k<WarshallM.size(); k++)
+                cout<<setw(4)<<"N"<<k;
+            cout<<endl;
+            for(int i=0; i<WarshallM.size(); i++) {
+                cout<<setw(4)<<"N"<<i;
+                for(int j=0; j<M[i].size(); j++) {
 
 
+                    if(WarshallM[i][j]==1)
+                        cout<<setw(4)<<"1";
+                    else if(WarshallM[i][j]==0)
+                        cout<<setw(4)<<"0";
+                    cout<<" ";
                 }
-            }
-            //设置DIS
-            for(int i=0; i<NodeNum; i++) {
-                for(int j=0; j<NodeNum; j++) {
-                    if(M[i][j].GetWeight()!=0) {
-                        Dis[i][j] = M[i][j].GetWeight();
-                        //cout<<i<<"to"<<j<<":"<<Dis[i][j]<<endl;
-
-                    }
-                }
-            }
-
-            //关键代码
-            for(int k=0; k<NodeNum; k++)
-                for(int i=0; i<NodeNum; i++)
-                    for(int j=0; j<NodeNum; j++) {
-
-                        if(Dis[i][k]+Dis[k][j]<Dis[i][j]) {
-                            Dis[i][j] = Dis[i][k]+Dis[k][j];
-                            //cout<<"Dis"<<i<<k<<"="<<Dis[i][k]<<endl;
-                            //cout<<"Dis"<<k<<j<<"="<<Dis[k][j]<<endl;
-                            //cout<<"Dis"<<i<<j<<"="<<Dis[i][j]<<endl;
-                            //cout<<"YES"<<endl;
-                            Path[i][j] = k;
-                        }
-                    }
-            //输出最短路
-            for(int i=0; i<NodeNum; i++)
-                for(int j=0; j<NodeNum; j++) {
-                    if(i!=j) {
-
-                        //输出最短路径
-                        if(Dis[i][j]!=INF) {
-                            cout<<"从"<<Nodes[i].getNodeName()<<"到"<<Nodes[j].getNodeName();
-                            cout<<"最短路径长度为:"<<Dis[i][j]<<endl;
-                            cout<< "从"<<Nodes[i].getNodeName()<<"到"<<Nodes[j].getNodeName()<<"最短路径";
-                            cout<<Nodes[i].getNodeName();
-                            prn_pass(i,j);
-                            cout<<"-->"<<Nodes[j].getNodeName()<<endl;
-                        } else if(Dis[i][j]==INF) {
-                            //cout<<Nodes[i].getNodeName()<<"与"<<Nodes[j].getNodeName()<<"不可达"<<endl;
-                        }
-                    }
-                }
-
-        }
-
-
-        void  prn_pass(int j , int k) {
-            if (Path[j][k]!=-1) {
-                prn_pass(j,Path[j][k]);
-                cout<<"-->"<<Path[j][k];
-                prn_pass(Path[j][k],k);
+                cout<<endl;
             }
         }
-
     private:
-
-
-
         int NodeNum;					//节点数
         vector<vector <GraphEdge> > M;	//vector二维数组
+        vector<vector <int> >WarshallM;	//Warshall矩阵存储可达性
         vector<GraphNode> Nodes;		//存储节点数组
-        vector<vector <int> >Path;
-        vector<vector <int> >Dis;
-
 };
 #endif
